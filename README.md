b073d31866cfb804dd7113e9d044f81f1f5e2134
Benjamin Young <byoung2@wiley.com>
2020-02-12 15:20:10 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Whois JSON CLI
+
+This is a simple command line interface (CLI) for dumping whois data out as
+JSON for use in document storage databases.
+
+## Usage
+
+```
+$ npm i -g
+$ whois-json example.com
+```
+
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+/**
+ * @copyright 2020 John Wiley & Sons, Inc.
+ * @license MIT
+ **/
+
+const whois = require('whois-json');
+
+require('yargs')
+  .scriptName("pirate-parser")
+  .usage('$0 <domain>', 'Search whois', (yargs) => {
+    yargs.positional('domain', {
+      type: 'string',
+      describe: 'domain name to search for in whois records'
+    });
+  }, async (argv) => {
+    var results = await whois(argv.domain);
+    console.log(JSON.stringify(results, null, 2));
+  })
+  .help()
+  .argv
